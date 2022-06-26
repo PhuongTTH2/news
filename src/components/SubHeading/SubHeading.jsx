537b550de1c0e1ab503692274619f0a859fc8530
phuongtth <phuongtth@kozo-japan.com>
2022-06-26 16:27:20 +0700

    fix warning

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -4,7 +4,7 @@ import {images} from '../../constants'
 const SubHeading = ({title}) => (
   <div>
     <p className='p__cormorant'>{title}</p>
-    <img src={ images.spoon}/>
+    <img src={ images.spoon} alt="img"/>
   </div>
 );
 
