84a29f21e784818f8a0bcd6f4a789f75d42386d6
phuongtth <phuongtth@kozo-japan.com>
2022-07-06 20:13:19 +0700

    fix folder

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,6 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-import "./assets/css/bootstrap.css";
-import "./assets/css/all.css";
-import "./assets/css/fonts.css";
-import "./assets/css/styles.css";
-import "./assets/css/responsive.css";
-import "./assets/datepicker/bootstrap-datetimepicker.css";
 import { Provider } from "react-redux";
 import store from "app/store";
 ReactDOM.render(
